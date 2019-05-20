ecdd6654fcb83982c9cdbe327cdc756f3c93f7a7
Robert Smidt <rsmidt909@hotmail.com>
2019-05-20 13:22:45 -0500

    creating repo

---
+++ NodeSearchAndAdd/BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeSearchAndAdd
+{
+    class BinaryTree
+    {
+        private Node start;
+        public int left;
+        public int right;
+
+
+        public void BinaryTreeStart()
+        {
+            start = null;
+        }
+
+        public void CreateTree(int data)
+        {
+
+            InsertAtEnd(data);
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node node;
+            Node temp = new Node(data);
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+            //-----------------------------------------------------------------------------------------
+            node = start;
+            if (temp.info <= node.info)
+            {
+                if (node.leftLink == null)
+                {
+                    node.leftLink = temp;
+                }
+                else { InsertAtEnd(data); }
+            }
+            if (node.rightLink == null)
+            {
+                node.rightLink = temp;
+            }
+            else { InsertAtEnd(data); }
+        }
+
+    }
+}
+}
---
+++ NodeSearchAndAdd/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeSearchAndAdd
+{
+    class Node
+    {
+        public int info;
+        public Node rightLink;
+        public Node leftLink;
+        public Node(int info)
+        {
+            this.info = info;
+            rightLink = null;
+            leftLink = null;
+        }
+    }
+}
